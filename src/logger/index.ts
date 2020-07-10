eb9047f091c163fd420d498d5aa9ad434cdba3b9
TomKristie <65685417+TomKristie@users.noreply.github.com>
2020-07-10 11:20:43 -0400

    feat: forking with octokit given repo and owner (#20)
    This does not yet support syncing an out-of-date fork

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,11 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {describe, it} from 'mocha';
-import {assert} from 'chai';
+import {Logger, Level} from 'pino';
+import * as Pino from 'pino';
+const logger = Pino();
 
-describe('Test', () => {
-  it('should pass', async () => {
-    assert.isTrue(true);
-  });
-});
+export {logger, Logger, Level};
---
+++ src/types/index.ts
@@ -0,0 +1,55 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Level, Logger} from '../logger';
+import {Octokit} from '@octokit/rest';
+
+// a flat object of the path of the file as the key, and the text contents as a value
+interface Files {
+  [index: string]: string;
+}
+
+/**
+ * The user parameter for GitHub data needed for creating a PR
+ */
+interface GitHubContextParam {
+  // the owner of the target fork repository
+  upstreamOwner: string;
+  // the name of the target fork repository
+  upstreamRepo: string;
+  // the name of the branch to push changes to
+  originBranch?: string;
+  // the description of the pull request
+  prDescription?: string;
+  // the title of the pull request
+  prTitle?: string;
+}
+
+/**
+ * GitHub data needed for creating a PR
+ */
+interface GitHubContext {
+  // the owner of the target fork repository
+  upstreamOwner: string;
+  // the name of the target fork repository
+  upstreamRepo: string;
+  // the name of the branch to push changes to
+  originBranch: string;
+  // the description of the pull request
+  prDescription: string;
+  // the title of the pull request
+  prTitle: string;
+}
+
+export {Files, GitHubContextParam, GitHubContext, Level, Logger, Octokit};
