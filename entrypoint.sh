6fb0f1f3949bbd31601ba554137ce12dff8a2056
Jeff Ching <chingor@google.com>
2020-10-05 13:39:48 -0700

    fix(action): default upstream owner/repo to the current repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,15 +16,25 @@
 
 set -e
 
-code-suggester $INPUT_COMMAND \
---upstream-repo="$INPUT_UPSTREAM_REPO" \
---upstream-owner="$INPUT_UPSTREAM_OWNER" \
---description="$INPUT_DESCRIPTION" \
---title="$INPUT_TITLE" \
---branch="$INPUT_BRANCH" \
---primary="$INPUT_PRIMARY" \
---message="$INPUT_MESSAGE" \
---force="$INPUT_FORCE" \
---maintainers-can-modify="$INPUT_MAINTAINERS_CAN_MODIFY" \
---git-dir="$INPUT_GIT_DIR" \
---fork="$INPUT_FORK"
+if [[ -z "${INPUT_UPSTREAM_REPO}" ]]
+then
+    INPUT_UPSTREAM_REPO=$(cat ${GITHUB_REPOSITORY} | cut -d/ -f2)
+fi
+
+if [[ -z "${INPUT_UPSTREAM_OWNER}" ]]
+then
+    INPUT_UPSTREAM_OWNER=$(cat ${GITHUB_REPOSITORY} | cut -d/ -f1)
+fi
+
+code-suggester ${INPUT_COMMAND} \
+    --upstream-repo="${INPUT_UPSTREAM_REPO}" \
+    --upstream-owner="${INPUT_UPSTREAM_OWNER}" \
+    --description="${INPUT_DESCRIPTION}" \
+    --title="${INPUT_TITLE}" \
+    --branch="${INPUT_BRANCH}" \
+    --primary="${INPUT_PRIMARY}" \
+    --message="${INPUT_MESSAGE}" \
+    --force="${INPUT_FORCE}" \
+    --maintainers-can-modify="${INPUT_MAINTAINERS_CAN_MODIFY}" \
+    --git-dir="${INPUT_GIT_DIR}" \
+    --fork="${INPUT_FORK}"
