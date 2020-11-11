1d3a5a3c1187da670725fd9c3ebb86a905a433b9
sofisl <55454395+sofisl@users.noreply.github.com>
2020-11-10 22:21:15 -0800

    build(to be reverted): add logging for flaky test continuous (#158)
    * add logging

--- src/bin/handle-git-dir-change.ts
+++ src/bin/handle-git-dir-change.ts
@@ -41,6 +41,7 @@ class InstallationError extends Error {
  */
 export function resolvePath(dir: string) {
   const absoluteDir = path.resolve(process.cwd(), dir);
+  console.log(`absolute directory in main ${absoluteDir}`);
   return absoluteDir;
 }
 
